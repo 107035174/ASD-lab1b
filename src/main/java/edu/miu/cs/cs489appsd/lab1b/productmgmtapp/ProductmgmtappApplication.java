a5525dc6cb2d2bfbc454a4d546afc5bf9e53fe34
Saikhanbat <saikhanbat.bayarsaikhan@miu.edu>
2024-04-02 13:26:43 -0500

    First commit to initiate git

---
+++ src/main/java/edu/miu/cs/cs489appsd/lab1b/productmgmtapp/ProductmgmtappApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489appsd.lab1b.productmgmtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmgmtappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmgmtappApplication.class, args);
+	}
+
+}
